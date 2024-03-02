db194cd0724a689d999af47e361451734f34d5e2
bander <bannadr1@hotmail.com>
2024-03-02 15:42:17 +0300

    origin, update k8s yaml with multi brokers

--- build_and_push_amd.sh
+++ build_and_push_amd.sh
@@ -4,7 +4,7 @@
 DOCKER_BUILDKIT=1 docker build --platform linux/amd64 . -t nodejs-origin-rdkafka-v4-amd64
 
 # Tag Docker image
-docker tag nodejs-origin-rdkafka-v4-amd64:latest bandersaeed94/la79y:origin-v2
+docker tag nodejs-origin-rdkafka-v4-amd64:latest bandersaeed94/la79y:origin-v3
 
 # Push Docker image to Docker Hub
-docker push bandersaeed94/la79y:origin-v2
+docker push bandersaeed94/la79y:origin-v3
