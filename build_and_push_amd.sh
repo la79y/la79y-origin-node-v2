220f9bb01a8e59f4499e5643f07f89f3da800172
bander <bannadr1@hotmail.com>
2024-05-02 12:31:26 +0300

    update docker image tage.

--- build_and_push_amd.sh
+++ build_and_push_amd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Enable BuildKit and build Docker image
-DOCKER_BUILDKIT=1 docker build --platform linux/amd64 . -t nodejs-origin-rdkafka-v5-amd64
+DOCKER_BUILDKIT=1 docker build --platform linux/amd64 . -t nodejs-origin-rdkafka-v6-amd64
 
 # Tag Docker image
-docker tag nodejs-origin-rdkafka-v5-amd64:latest bandersaeed94/la79y:origin-v5
+docker tag nodejs-origin-rdkafka-v6-amd64:latest bandersaeed94/la79y:origin-v6
 
 # Push Docker image to Docker Hub
-docker push bandersaeed94/la79y:origin-v5
+docker push bandersaeed94/la79y:origin-v6
